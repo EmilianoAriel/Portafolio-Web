f970b4d6e97210750f77806a68a60b05930e7c7e
Emiliano <emilianoariel6@gmail.com>
2024-12-26 12:20:38 -0300

    Primero comit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const lenguages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "nav.proyectos": "Proyectos",
+    "nav.experiencia": "Experiencia",
+    "nav.habilidades": "Habilidades",
+    "nav.sobremi": "Sobre mi",
+  },
+  en: {
+    "nav.proyectos": "Projects",
+    "nav.experiencia": "Experience",
+    "nav.habilidades": "Skills",
+    "nav.sobremi": "About me",
+  },
+} as const;
